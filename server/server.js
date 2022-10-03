f06bc07d6ff84d5bd965af96fe154d0da1cd7741
1wes <okemwawes@gmail.com>
2022-10-03 14:36:24 +0300

    wholesale initial commit

---
+++ server/server.js
@@ -0,0 +1,66 @@
+const express=require('express');
+const cors=require('cors');
+const mysql=require('mysql');
+
+const app=express();
+
+const port=5000;
+
+// use json to parse the incoming requests
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+
+// create a connection to the databse
+const db_conn=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database:'reactnodeapp'
+});
+
+// make the connection
+db_conn.connect(err=>{
+    if (err){
+        throw err
+    }
+
+    console.log("Mysql has successfully connected....");
+});
+
+// // to be replaced by actual data from the database;
+// const username='Wes';
+
+// const password="Okemwa";
+
+app.post("/server/intro", (req, res)=>{
+    // let userDetails=req.body;
+    let user_data={
+        username:req.body.username,
+        password:req.body.password
+    }
+
+    // insert the details into the database
+    let insert_sql="INSERT INTO users SET ?";
+
+    let query=db_conn.query(insert_sql, user_data, (err, result)=>{
+
+        if(err){
+            throw err
+        }
+
+        console.log(`Check out the result object: ${result}`)
+    })
+
+})
+
+app.get("/server/intro", (req, res)=>{
+    const status=res.status(200);
+
+    status.send("This was sent from the back-end ..Node via the express web framework");
+});
+
+app.listen(port, ()=>{
+    console.log(`The server is listening at port ${port}`);
+});
+
--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
 import './App.css';
+// import React, {useEffect} from 'react';
+import React from 'react';
+import axios from './baseUrl';
+import logo from './kabulogo-removebg-preview.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+
+    this.state={
+      username:'', 
+      password:''
+    }
+
+    this.handleUsernameChange=this.handleUsernameChange.bind(this);
+    this.handlePasswordChange=this.handlePasswordChange.bind(this);
+    this.handleSubmit=this.handleSubmit.bind(this);
+  }
+
+  // handleFetch=()=>{
+  //   axios.get('/server/intro').then(response=>{
+  //     this.setState({
+  //       backendData:response.data
+  //     })
+  //   })
+  // }
+
+  handleUsernameChange=event=>{
+    this.setState({
+      username:event.target.value
+    })
+  }
+
+  handlePasswordChange=event=>{
+    this.setState({
+      password:event.target.value
+    })
+  }
+
+  handleSubmit=e=>{
+
+    e.preventDefault();
+
+    const userDetails={
+      username:this.state.username,
+      password:this.state.password
+    }
+
+    this.setState({
+      username:'',
+      password:''
+    })
+    
+    // post the data to the backend
+    axios.post("/server/intro", userDetails).then(res=>{
+      alert(res.data);
+    }).catch(err=>{
+      console.log(err)
+
+      // clear the state after submitting the details
+
+    })
+  }
+
+  render(){
+
+    return(
+      <div className='login'>
+        <div className='login-form'> 
+          <div className='logo'>
+            <img src={logo} alt="Kabarak-university-logo" />
+          </div>
+            <h3>
+              Kabarak University Students Portal
+            </h3>
+
+            <div className='login-details'>
+              <form onSubmit={this.handleSubmit}>
+                <label>
+                  Username
+                </label>
+                <input type="text" placeholder='Enter your username' value={this.state.username} onChange={this.handleUsernameChange}>
+                </input>
+
+                <br></br>
+
+                <label>
+                  Password
+                </label>
+                <input type="password" placeholder='Enter your password' value={this.state.password} onChange={this.handlePasswordChange}>
+                </input>
+
+                <p className="forgot-password">
+                  Forgot password?
+                </p>
+
+                <div className='submit-button'>
+                  <button type='submit' value='Submit'>
+                    Login
+                  </button>
+                </div>
+              </form>
+            </div>
+        </div>
+      </div>
+    )
+  }
 }
-
 export default App;
+

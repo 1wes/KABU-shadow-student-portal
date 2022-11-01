ac77a0683328fb31c6d717e4858b2a9c71003ebe
1wes <okemwawes@gmail.com>
2022-11-01 16:55:31 +0300

    use switch case instead of if statement in changing port number based on node environment

--- server/env-config.js
+++ server/env-config.js
@@ -1,5 +1,21 @@
 require('dotenv').config();
 
+switch(process.env.NODE_ENV){
+
+    case 'development':
+
+        process.env.PORT=5000;
+
+        break;
+    
+    case 'production':
+
+        process.env.PORT=5500;
+
+        break;
+
+}
+
 process.env.NODE_ENV=='development' ? process.env.PORT=5000 : process.env.PORT=5500
 
 module.exports={
@@ -8,4 +24,5 @@ module.exports={
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
     database:process.env.DB_NAME,
+    node_env:process.env.NODE_ENV
 }

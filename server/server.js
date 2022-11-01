8da1d17c133e18fcdfe4eafe461adfb3fac8f684
1wes <okemwawes@gmail.com>
2022-11-01 16:56:53 +0300

    dynamically change server listening port based on node environment

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express=require('express');
-const {port}= require('./env-config');
+const {port, node_env}= require('./env-config');
 const app=express();
 const cors=require('cors');
 const student=require('./login');
@@ -11,5 +11,5 @@ app.use(cors());
 app.use("/student", student);
 
 app.listen(port, ()=>{
-    console.log(`The server is listening at port ${port}`);
+    console.log(node_env=='production' || node_env=='development' ? `The server is running at port ${port}` : `Please select your Node environment`);
 });

8ba66dcefa5544c4744a2a737c74cb303962e198
1wes <okemwawes@gmail.com>
2022-11-01 16:15:32 +0300

    check node environment and adjust server listening port accordingly

--- server/env-config.js
+++ server/env-config.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 
+process.env.NODE_ENV=='development' ? process.env.PORT=5000 : process.env.PORT=5500
+
 module.exports={
     port:process.env.PORT,
     host:process.env.DB_HOST,
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME
+    database:process.env.DB_NAME,
 }

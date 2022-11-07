6ae92909ec1805747e84b36ca09ebab83f6108b1
1wes <okemwawes@gmail.com>
2022-11-07 18:15:51 +0300

    change password comparison function name to comparePassword

--- server/utils/passwordMatch.js
+++ server/utils/passwordMatch.js
@@ -1,9 +1,9 @@
 const bcrypt=require('bcrypt');
 
-let passwordMatch= async (input_password, stored_password)=>{
+let comparePassword= async (input_password, stored_password)=>{
     
     return await bcrypt.compare(input_password, stored_password)
 
 }
 
-module.exports=passwordMatch;
+module.exports=comparePassword;

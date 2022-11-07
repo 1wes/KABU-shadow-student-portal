a1b538a2d78d1749fede1b81eb672978d9cbf058
1wes <okemwawes@gmail.com>
2022-11-07 15:31:41 +0300

    write a function to generate a token based on student reg number

---
+++ server/authentication/generateToken.js
@@ -0,0 +1,10 @@
+const jwt=require('jsonwebtoken');
+const {token_secret_key}=require('../env-config');
+
+let secret_key=token_secret_key;
+
+const generateToken=(reg_no)=>{
+    return jwt.sign(reg_no, secret_key, {expiresIn:'1h'});
+}
+
+exports.module=generateToken;

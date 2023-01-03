444fa5919c3ba6ac3161e37300c1d2587d856a2a
1wes <okemwawes@gmail.com>
2023-01-03 16:24:41 +0300

    add token verification route to server

--- server/server.js
+++ server/server.js
@@ -2,13 +2,15 @@ const express=require('express');
 const {port, node_env}= require('./env-config');
 const app=express();
 const cors=require('cors');
-const student=require('./login');
+const login=require('./login');
+const auth=require('./middleware/auth');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
 
-app.use("/student", student);
+app.use("/student", login);
+app.use("/student", auth)
 
 app.listen(port, ()=>{
     console.log(node_env=='production' || node_env=='development' ? `The server is running at port ${port}` : `Please select your Node environment`);

209235191deb733dbc7a70c96616becd1174be1e
1wes <okemwawes@gmail.com>
2023-01-10 16:02:25 +0300

    parse cookies

--- server/server.js
+++ server/server.js
@@ -4,10 +4,12 @@ const app=express();
 const cors=require('cors');
 const login=require('./login');
 const auth=require('./middleware/auth');
+const cookieParser=require('cookie-parser');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
+app.use(cookieParser());
 
 app.use("/student", login);
 app.use("/student", auth)

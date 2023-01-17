9813e8b5a4d63c15bb739ade937d2527cdfcbe9f
1wes <okemwawes@gmail.com>
2023-01-17 16:05:19 +0300

    destructure secret key from environment variables json object

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,17 +1,17 @@
 const express=require('express');
 const router=express.Router();
 const jwt=require('jsonwebtoken');
-const token_secret_key=require('../env-config');
+const {token_secret_key}=require('../env-config');
 
 router.post("/auth", (req, res)=>{
 
     const token=req.headers['auth_token'];
-
+    
     if(token){
 
         console.log(token);
 
-        const validToken=jwt.verify(token, JSON.stringify(token_secret_key));
+        const validToken=jwt.verify(token, token_secret_key);
 
         if(validToken){
             res.status(200).send("Login validated");

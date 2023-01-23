bf40e8cb57892df9f60cadceb251d4f843c4a093
1wes <okemwawes@gmail.com>
2023-01-23 16:02:19 +0300

    synchronize UI with corresponding URL

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 // get the root of the app
 const root=ReactDOM.createRoot(document.getElementById('root'));
 
 // render the page
 root.render(
-  <App />
+  <Router>
+    <Routes>
+      <Route path='/' element={<App />}></Route>
+    </Routes>
+  </Router>
 )

354995cc10c2a98c2bbcb4c400d57e39786ceabc
1wes <okemwawes@gmail.com>
2023-01-31 17:43:08 +0300

    pass message prop to modal component

--- src/components/password-reset.js
+++ src/components/password-reset.js
@@ -4,6 +4,8 @@ import {Link} from 'react-router-dom';
 import Footnote from './footnote';
 import Modal from './modal';
 
+let warningMessage="Enter Registration Number"
+
 function Centeredsegment(props){
 
     return(
@@ -119,7 +121,7 @@ class Resetpassword extends React.Component{
                 </Centeredsegment>
 
                 <Footnote/>
-                <Modal/>
+                <Modal message={warningMessage} />
             </div>
             </React.Fragment>
         )

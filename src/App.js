25ed0f3cd2de1b0437b2cac6c11d148e0f7f659d
1wes <okemwawes@gmail.com>
2023-02-09 15:07:20 +0300

    lift state up to the App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-// import React, {useEffect} from 'react';
 import React from 'react';
 import axios from './baseUrl';
 import {Link} from 'react-router-dom';
@@ -19,28 +18,23 @@ class Loginform extends React.Component{
   constructor(props){
     super(props);
 
-    this.handleSubmit=this.handleSubmit.bind(this);
     this.handleRegChange=this.handleRegChange.bind(this);
     this.handlePasswordChange=this.handlePasswordChange.bind(this);
   }
 
-  handleSubmit=(event)=>{
-    this.props.handleSubmit(event.target.value)
+  handleRegChange=(event)=>{
+    this.props.handleRegChange(event.target.value);
   }
 
-  handleRegChange=()=>{
-    this.props.handleRegChange();
-  }
-
-  handlePasswordChange=()=>{
-    this.props.handlePasswordChange();
+  handlePasswordChange=(event)=>{
+    this.props.handlePasswordChange(event.target.value);
   }
 
   render(){
 
     return(
       <React.Fragment>
-        <form onSubmit={this.handleSubmit} className='login-form'>
+        <form className='login-form'>
           <label>
             Reg. Number
           </label>
@@ -68,7 +62,6 @@ class Loginform extends React.Component{
                 Remember me
               </label>
             </span>
-            {/* <label>Remember me</label> */}
           </div>
         </form>
       </React.Fragment>
@@ -89,79 +82,64 @@ function Forgotpasswordlink(props){
 
 class App extends React.Component{
 
-  // constructor(props){
-  //   super(props);
+  constructor(props){
+    super(props);
 
-  //   this.state={
-  //     reg_no:'', 
-  //     password:''
-  //   }
+    this.state={
+      reg_no:'',
+      password:''
+    }
 
-  //   this.handleRegChange=this.handleRegChange.bind(this);
-  //   this.handlePasswordChange=this.handlePasswordChange.bind(this);
-  //   this.handleSubmit=this.handleSubmit.bind(this);
-  //   this.togglePasswordVisibility=this.togglePasswordVisibility.bind(this);
-  // }
+    this.submitForm=this.submitForm.bind(this);
+    this.handleRegChange=this.handleRegChange.bind(this);
+    this.handlePasswordChange=this.handlePasswordChange.bind(this);
+  }
 
-  // handleRegChange=event=>{
-  //   this.setState({
-  //     reg_no:event.target.value
-  //   })
-  // }
+  handleRegChange=(newRegNo)=>{
+    this.setState({
+      reg_no:newRegNo
+    });
+  }
 
-  // handlePasswordChange=event=>{
-  //   this.setState({
-  //     password:event.target.value
-  //   })
-  // }
+  handlePasswordChange=(newPassword)=>{
+    this.setState({
+      password:newPassword
+    });    
+  }
 
-  // handleSubmit=e=>{
+  submitForm=e=>{
 
-  //   e.preventDefault();
+    e.preventDefault();
 
-  //   const userDetails={
-  //     reg_no:this.state.reg_no,
-  //     password:this.state.password
-  //   }
+    const userDetails={
+      reg_no:this.state.reg_no,
+      password:this.state.password
+    }
 
-  //   this.setState({
-  //     reg_no:'',
-  //     password:''
-  //   })
-    
-  //   // post the data to the backend
-  //   axios.post("/student/login", userDetails).then(res=>{
+    // post the data to the backend
+    axios.post("/student/login", userDetails).then(res=>{
 
-  //     if(res.status===201){
+      if(res.status===201){
 
-  //       const headersConfig={
-  //         headers:{
-  //           'auth_token':res.data.responseData.token
-  //         }
-  //       }
+        const headersConfig={
+          headers:{
+            'auth_token':res.data.responseData.token
+          }
+        }
 
-  //       axios.post("/student/auth", '', headersConfig).then(res=>{
-  //         alert(JSON.stringify(res.status));
-  //       }).catch(err=>{
-  //         alert(err.response.status);
-  //       })
+        axios.post("/student/auth", '', headersConfig).then(res=>{
+          alert(JSON.stringify(res.status));
+        }).catch(err=>{
+          alert(err.response.status);
+        })
 
-  //     }
+      }
 
-  //   }).catch(err=>{
-  //     alert(err.response.status)
-  //     // clear the state after submitting the details
-  //   });
-  // }
-
-  // togglePasswordVisibility=(e)=>{
-  //   let passwordField=document.getElementById('show-password');
-
-  //   const type=passwordField.getAttribute('type')==='password'? 'text':'password'
-
-  //   passwordField.setAttribute('type', type);
-
-  // }
+    }).catch(err=>{
+      alert(err.response.status)
+      // clear the state after submitting the details
+    });
+  }
 
   render(){
 
@@ -178,11 +156,11 @@ class App extends React.Component{
               <hr/>
 
               <Login>
-                <Loginform/>
+                <Loginform handlePasswordChange={this.handlePasswordChange} handleRegChange={this.handleRegChange} />
               </Login>
 
               <div className='sub-btn-div'>
-                <SubmitButton text={text} />
+                <SubmitButton text={text} submitForm={this.submitForm} />
               </div>
 
               <Forgotpasswordlink/>
@@ -193,49 +171,6 @@ class App extends React.Component{
         </div>
         
       </React.Fragment>
-      // <div className='login'>
-      //   <div className='login-form'> 
-      //     <div className='logo'>
-      //       <img src={logo} alt="Kabarak-university-logo" />
-      //     </div>
-      //       <h3>
-      //         Kabarak University Students Portal
-      //       </h3>
-
-      //       <div className='login-details'>
-      //         <form onSubmit={this.handleSubmit}>
-      //           <label>
-      //             Registration Number
-      //           </label>
-      //           <input type="text" placeholder='Enter your reg. number' value={this.state.reg_no} onChange={this.handleRegChange} required>
-      //           </input>
-
-      //           <br></br>
-
-      //           <label>
-      //             Password
-      //           </label>
-      //           <input type="password"  id='show-password' placeholder='Enter your password' value={this.state.password} onChange={this.handlePasswordChange} required>
-      //           </input>
-
-      //           <div className='password-visibility'>
-      //             <input type="checkbox" onClick={this.togglePasswordVisibility}></input>
-      //             <label>Show password</label>
-      //           </div>
-
-      //           <p className="forgot-password">
-      //             <Link to='/student/login/forgotPassword' id='forgot-password-link'>Forgot password?</Link>
-      //           </p>
-
-      //           <div className='submit-button'>
-      //             <button type='submit' value='Submit'>
-      //               Login
-      //             </button>
-      //           </div>
-      //         </form>
-      //       </div>
-      //   </div>
-      // </div>
     )
   }
 }

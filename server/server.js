cb87c4a5090f07aeda20f05938983aafa4288185
1wes <okemwawes@gmail.com>
2023-03-06 17:36:05 +0300

    add forgotPassword middleware route

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ const express=require('express');
 const {port, node_env}= require('./env-config');
 const app=express();
 const cors=require('cors');
+const cookieParser=require('cookie-parser');
 const login=require('./login');
 const auth=require('./middleware/auth');
-const cookieParser=require('cookie-parser');
+const forgotPassword=require('./middleware/reset-password');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -13,6 +14,7 @@ app.use(cookieParser());
 
 app.use("/student", login);
 app.use("/student", auth)
+app.use("/student", forgotPassword);
 
 app.listen(port, ()=>{
     console.log(node_env=='production' || node_env=='development' ? `The server is running at port ${port}` : `Please select your Node environment`);

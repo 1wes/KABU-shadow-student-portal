cbc62ccb731310967a66520efde3bbbbf90a9596
1wes <okemwawes@gmail.com>
2023-04-14 19:01:57 +0300

    create mail transporter using SMTP

---
+++ server/utils/mailer.js
@@ -0,0 +1,19 @@
+const nodemailer=require('nodemailer');
+const{email, client_id, client_secret, refresh_token, access_token}=require('../env-config');
+const recipient=require('../middleware/reset-password');
+
+console.log(recipient);
+
+const transporter=nodemailer.createTransport({
+    host:'smtp.gmail.com', 
+    auth:{
+        user:email,
+        type:'OAuth2',
+        clientId:client_id,
+        clientSecret:client_secret,
+        refreshToken:refresh_token,
+        accessToken:access_token,
+    }
+});
+
+module.exports=transporter

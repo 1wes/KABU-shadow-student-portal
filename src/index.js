f4d61399b9bd8622899478264e15f6dd065e3af3
1wes <okemwawes@gmail.com>
2023-04-18 20:07:23 +0300

    add resetpassword route

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Resetpassword from './components/forgot-password';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Enterpassword from './components/reset-password'
 
 // get the root of the app
 const root=ReactDOM.createRoot(document.getElementById('root'));
@@ -14,6 +15,7 @@ root.render(
     <Routes>
       <Route path='/' element={<App/>}></Route>
       <Route path='/student/login/forgotPassword' element={<Resetpassword/>}></Route>
+      <Route path='/student/login/forgotPassword/resetPassword' element={<Enterpassword/>}></Route>
     </Routes>
   </Router>
 )

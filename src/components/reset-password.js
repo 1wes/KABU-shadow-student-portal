1dfb97ae2547fa161bed8b088167491add50def9
1wes <okemwawes@gmail.com>
2023-04-19 18:19:44 +0300

    import and reuse universal centered form input segment

---
+++ src/components/reset-password.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {Centeredsegment, Contentsegment, Logobanner, SubmitButton, Footnote} from './forgot-password';
+import './reset-password.css';
+
+class EnterNewPassword extends React.Component{
+
+    render(){
+
+        return(
+            <React.Fragment>
+                <div className='change-password'>
+                    <Centeredsegment>
+                        <Contentsegment>
+                            <Logobanner/>
+                            <hr/>
+
+                        
+                        </Contentsegment>
+                    </Centeredsegment>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+export default EnterNewPassword;

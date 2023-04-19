86d51e47d061c7572266f864880d312f54bc7c46
1wes <okemwawes@gmail.com>
2023-04-19 18:56:09 +0300

    add input fields to form

--- src/components/reset-password.js
+++ src/components/reset-password.js
@@ -3,13 +3,31 @@ import {Link} from 'react-router-dom';
 import {Centeredsegment, Contentsegment, Logobanner, SubmitButton, Footnote} from './forgot-password';
 import './reset-password.css';
 
-class EnterNewPassword extends React.Component{
+let submitMessage=`Change Password`;
+
+class EnterNewPasswordForm extends React.Component{
 
     render(){
 
         return(
             <React.Fragment>
-                
+                <form className='change-password-form'>
+                    <label>
+                        Enter New Password
+                    </label>
+
+                    <div className='new-password'>
+                        <input type='text' placeholder='Enter New Password' onChange={this.handleNewPassword}></input>
+                    </div>
+
+                    <label>
+                        Repeat Your Password
+                    </label>
+
+                    <div className='repeat-password'>
+                        <input type='text' placeholder='Repeat Your Password' onChange={this.handleRepeatPassword}></input>
+                    </div>
+                </form>
             </React.Fragment>
         )
     }
@@ -26,10 +44,14 @@ class Changepassword extends React.Component{
                         <Contentsegment>
                             <Logobanner/>
                             <hr/>
+                            <EnterNewPasswordForm/>
 
-                        
+                            <div className='change-passwd-sbmt-btn'>
+                                <SubmitButton buttonMessage={submitMessage} submitForm={this.submitForm} />
+                            </div>
                         </Contentsegment>
                     </Centeredsegment>
+                    <Footnote/>
                 </div>
             </React.Fragment>
         )

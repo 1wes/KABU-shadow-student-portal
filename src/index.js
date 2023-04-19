4526dc093c7a5c71ee7d90537f36eceafdf64473
1wes <okemwawes@gmail.com>
2023-04-19 18:22:39 +0300

    change imported component name to Changepassword

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import Resetpassword from './components/forgot-password';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Enterpassword from './components/reset-password'
+import Changepassword from './components/reset-password'
 
 // get the root of the app
 const root=ReactDOM.createRoot(document.getElementById('root'));
@@ -15,7 +15,7 @@ root.render(
     <Routes>
       <Route path='/' element={<App/>}></Route>
       <Route path='/student/login/forgotPassword' element={<Resetpassword/>}></Route>
-      <Route path='/student/login/forgotPassword/resetPassword' element={<Enterpassword/>}></Route>
+      <Route path='/student/login/forgotPassword/resetPassword' element={<Changepassword/>}></Route>
     </Routes>
   </Router>
 )

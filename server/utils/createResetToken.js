95cf112ba58cff0f6ce8924f4846dbf3a1a0e295
1wes <okemwawes@gmail.com>
2023-04-24 16:14:51 +0300

    add timestamp of token creation to function return variable

--- server/utils/createResetToken.js
+++ server/utils/createResetToken.js
@@ -2,7 +2,7 @@ const crypto=require('crypto');
 
 let generateResetToken=()=>{
 
-    return token=crypto.randomInt(1, 1000000000).toString();
+    return token=`${crypto.randomInt(1, 1000000000).toString()}+${Date.now()}`;
 
 }
 module.exports=generateResetToken;

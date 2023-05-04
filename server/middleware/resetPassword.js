fab6dfe6dea5e0617ffd966363d72343895fceba
1wes <okemwawes@gmail.com>
2023-05-04 22:07:35 +0300

    verify token before resetting password

--- server/middleware/resetPassword.js
+++ server/middleware/resetPassword.js
@@ -1,16 +1,39 @@
 const express=require('express');
+const app=express();
 const router=express.Router();
 const {decryptToken, encryptToken}=require('../utils/resetTokenOps');
+const con=require('../database');
+const tokenVerifier=require('./auth');
 
-router.post('/login/forgotPassword/resetPassword', (req, res)=>{
+app.use(tokenVerifier)
 
-    let referringURL=req.headers.referer;
+router.post('/login/forgotPassword/resetPassword', tokenVerifier, (req, res)=>{
 
-    let userInfo=referringURL.split('?')[1].split('=')
+    if(statusCode==200){
+        let referringURL=req.headers.referer;
 
-    let user=userInfo[1].split('&')[0];
+        let userInfo=referringURL.split('?')[1].split('=')
 
-    let token=userInfo[2];
+        let user=userInfo[1].split('&')[0];
 
+        let token=userInfo[2];
+
+        let getTokeninfo=`SELECT * from students where pwd_reset_token='${token}'`;
+
+        con.query(getTokeninfo, (err, result)=>{
+
+            if(err){
+                throw err;
+            }
+
+            result?
+        
+            // match the token with the retrieved reg number;
+            result[0].reg_no==user?
+            console.log("match"):
+            res.sendStatus(403):res.sendStatus(404);
+
+        })
+    }
 })
 module.exports=router;

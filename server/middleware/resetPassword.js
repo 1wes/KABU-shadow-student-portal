119995d0107f06e73806e804021774fa581580e2
1wes <okemwawes@gmail.com>
2023-05-04 22:59:21 +0300

    pull user details based on search parameters pulled from request origin

--- server/middleware/resetPassword.js
+++ server/middleware/resetPassword.js
@@ -4,13 +4,17 @@ const router=express.Router();
 const {decryptToken, encryptToken}=require('../utils/resetTokenOps');
 const con=require('../database');
 const tokenVerifier=require('./auth');
+const { token_validity } = require('../env-config');
 
 app.use(tokenVerifier)
 
 router.post('/login/forgotPassword/resetPassword', tokenVerifier, (req, res)=>{
 
     if(statusCode==200){
+
         let referringURL=req.headers.referer;
+
+        let {confirmPassword}=req.body;
 
         let userInfo=referringURL.split('?')[1].split('=')
 
@@ -26,12 +30,25 @@ router.post('/login/forgotPassword/resetPassword', tokenVerifier, (req, res)=>{
                 throw err;
             }
 
-            result?
-        
-            // match the token with the retrieved reg number;
-            result[0].reg_no==user?
-            console.log("match"):
-            res.sendStatus(403):res.sendStatus(404);
+            if(result){
+
+                // match the token with the retrieved reg number;
+                if(result[0].reg_no==user){
+                    
+                }else{
+                    res.sendStatus(403);
+                }
+
+            }else{
+                res.sendStatus(404);
+            }
+            
+            // // check whether the token has expired, current limit is 24hrs;
+            // timeDiff=Date.now()-result[0].pwd_reset_token_timestamp
+
+            // timeDiff>token_validity?
+
+            // :res.sendStatus(403):res.sendStatus(404);
 
         })
     }

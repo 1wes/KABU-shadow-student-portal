fb68f74b5af823a6672145d914ae1448602c332f
1wes <okemwawes@gmail.com>
2023-05-04 18:54:33 +0300

    import initialization vector from env variables

--- server/utils/resetTokenOps.js
+++ server/utils/resetTokenOps.js
@@ -1,9 +1,7 @@
 const crypto=require('crypto');
-const {algorithm, security_key,}=require('../env-config');
+const {algorithm, security_key,iv}=require('../env-config');
 
 let encryptToken=(token)=>{
-
-    const iv=crypto.randomBytes(16);
 
     const cipher=crypto.createCipheriv(algorithm, security_key, iv);
 

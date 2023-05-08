aa1d85093de3d45532b601b0c14a6f39c125babe
1wes <okemwawes@gmail.com>
2023-05-08 19:15:52 +0300

    use switch case to avoid deep nesting issues

--- server/middleware/forgot-password.js
+++ server/middleware/forgot-password.js
@@ -12,65 +12,81 @@ app.use(tokenVerifier);
 
 router.post("/forgotPassword", tokenVerifier, (req, res)=>{
 
-    if (statusCode==200){
+    switch (statusCode){
+
+        case 200:
+            console.log(tokenInfo);
         
-        let {reg}=req.body;
+            let {reg}=req.body;
 
-        let getEmail=`SELECT * from students WHERE reg_no=?`;
+            let getEmail=`SELECT * from students WHERE reg_no=?`;
 
-        con.query(getEmail,reg, (err, result)=>{
-
-            if(err){
-                throw err
-            }
-
-            let resetToken=token();
-
-            let urlToken=resetToken.split('+');
-
-            let urlTokenTimestamp=urlToken[1];
-
-            let encryptedToken=encryptToken(resetToken);
-
-            let storeResetToken=`UPDATE students set pwd_reset_token=${urlToken[0]}, pwd_reset_token_timestamp=${urlTokenTimestamp}, enc_pwd_reset_token='${encryptedToken}' WHERE reg_no='${result[0].reg_no}'`;
-
-            con.query(storeResetToken, (err, result)=>{
-
-                if(err){
-                    throw err;
-                }
-
-                console.log("Successfully updated password reset credentials !!!")
-            } )
-
-            let subject=`KABARAK PORTAL RESET PASSWORD LINK`;
-            let name=`${result[0].surname} ${result[0].first_name} ${result[0].last_name}`;
-            let capitalizedName=name.toUpperCase();
-            const finalUrl=new URL(pwd_reset_link);
-
-            finalUrl.searchParams.append("user", `${result[0].reg_no}`);
-            finalUrl.searchParams.append("Token", `${urlToken[0]}`);
-
-            const mailOptions={
-                from:email,
-                to:result[0].email,
-                subject:subject,
-                html:`Hi ${capitalizedName}.<br/>Kindly click <a href=${finalUrl.href.replace(/%2F/g, '/')}>here</a> to reset your password. <hr /> Note that this is an auto generated email.`+
-                " Kindly do not reply to it. <br/> <br/>"+
-                "Incase of any challenges, please contact Admission office for assistance. <br/> Contact Email : <a href='mailto=okemwawes@gmail.com'>okemwawes@gmail.com</a>"+
-                "<br/> <br/> Best Regards."
-            }
-
-            transporter.sendMail(mailOptions, (err, info)=>{
+            con.query(getEmail,reg, (err, result)=>{
 
                 if(err){
                     throw err
                 }
 
-                res.send(result[0].email);
+                let resetToken=token();
 
+                let urlToken=resetToken.split('+');
+
+                let urlTokenTimestamp=urlToken[1];
+
+                let encryptedToken=encryptToken(resetToken);
+
+                let storeResetToken=`UPDATE students set pwd_reset_token=${urlToken[0]}, pwd_reset_token_timestamp=${urlTokenTimestamp}, enc_pwd_reset_token='${encryptedToken}' WHERE reg_no='${result[0].reg_no}'`;
+
+                con.query(storeResetToken, (err, result)=>{
+
+                    if(err){
+                        throw err;
+                    }
+
+                    console.log("Successfully updated password reset credentials !!!")
+                } )
+
+                let subject=`KABARAK PORTAL RESET PASSWORD LINK`;
+                let name=`${result[0].surname} ${result[0].first_name} ${result[0].last_name}`;
+                let capitalizedName=name.toUpperCase();
+                const finalUrl=new URL(pwd_reset_link);
+
+                finalUrl.searchParams.append("user", `${result[0].reg_no}`);
+                finalUrl.searchParams.append("Token", `${urlToken[0]}`);
+
+                const mailOptions={
+                    from:email,
+                    to:result[0].email,
+                    subject:subject,
+                    html:`Hi ${capitalizedName}.<br/>Kindly click <a href=${finalUrl.href.replace(/%2F/g, '/')}>here</a> to reset your password. <hr /> Note that this is an auto generated email.`+
+                    " Kindly do not reply to it. <br/> <br/>"+
+                    "Incase of any challenges, please contact Admission office for assistance. <br/> Contact Email : <a href='mailto=okemwawes@gmail.com'>okemwawes@gmail.com</a>"+
+                    "<br/> <br/> Best Regards."
+                }
+
+                transporter.sendMail(mailOptions, (err, info)=>{
+
+                    if(err){
+                        throw err
+                    }
+
+                    res.send(result[0].email);
+                })
             })
-        })
+
+            break;
+
+        case 401:
+
+            console.log(statusCode+"\n "+message);
+
+            break;
+
+        case 403:
+
+            console.log("Unauthorized");
+
+            break;
     }
-})
+}) 
 module.exports=router

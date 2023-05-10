7dfc043832f4d7687ccfeed0dd07cc5a3f24b514
1wes <okemwawes@gmail.com>
2023-05-10 18:49:30 +0300

    add dashboard page route

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import Resetpassword from './components/forgot-password';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Changepassword from './components/reset-password'
+import Changepassword from './components/reset-password';
+import Dashboard from './components/dashboard';
 
 // get the root of the app
 const root=ReactDOM.createRoot(document.getElementById('root'));
@@ -16,6 +17,7 @@ root.render(
       <Route path='/' element={<App/>}></Route>
       <Route path='/student/login/forgotPassword' element={<Resetpassword/>}></Route>
       <Route path='/student/login/forgotPassword/resetPassword' element={<Changepassword/>}></Route>
+      <Route path='/student/dashboard' element={<Dashboard/>}></Route>
     </Routes>
   </Router>
 )

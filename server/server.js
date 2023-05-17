7d7bc5ce3b5aca68be18fad162c42288e627a3ff
1wes <okemwawes@gmail.com>
2023-05-17 12:15:12 +0300

    add user logout router middleware

--- server/server.js
+++ server/server.js
@@ -6,7 +6,8 @@ const cookieParser=require('cookie-parser');
 const login=require('./login');
 const forgotPassword=require('./middleware/forgot-password');
 const resetPassword=require('./middleware/resetPassword');
-const loginAuth=require('./middleware/login-auth')
+const loginAuth=require('./middleware/login-auth');
+const logoutUser=require('./middleware/logout');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -16,7 +17,8 @@ app.use(cookieParser());
 app.use("/student", login);
 app.use("/student", forgotPassword);
 app.use("/student", resetPassword);
-app.use("/student/", loginAuth)
+app.use("/student/", loginAuth);
+app.use("/student", logoutUser);
 
 app.listen(port, ()=>{
     console.log(node_env=='production' || node_env=='development' ? `The server is running at port ${port}` : `Please select your Node environment`);

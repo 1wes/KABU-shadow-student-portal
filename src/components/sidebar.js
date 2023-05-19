c4c295512829d31094da1f3002b011df86330ce1
1wes <okemwawes@gmail.com>
2023-05-19 14:41:59 +0300

    add sidebar menu items

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './sidebar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faArrowsDownToLine, faBed, faCloudArrowDown, faCodePullRequest, faFolderMinus, faGear, faNoteSticky, faRegistered, faSterlingSign, faUser, faYenSign } from '@fortawesome/free-solid-svg-icons';
 
 class Sidebar extends React.Component{
 
@@ -14,70 +14,125 @@ class Sidebar extends React.Component{
         return(
             <React.Fragment>
                 <div className='sidebar-content'>
-                    <div className='sidebar-content-title' id='dashboard-title'>
+                    <div className='sidebar-menu-title' id='dashboard-title'>
                         <label>
                             Dashboard
                         </label>
 
                         <li>
-
                             <i>
                                 <FontAwesomeIcon icon={faUser} />
                             </i>
-                            Personal Profile
+                            <span className='sidebar-menu-item'>Personal Profile</span>
                         </li>
                     </div>
 
-                    <div className='sidebar-content-title' id='academics-title'>
+                    <div className='sidebar-menu-title' id='academics-title'>
                         <label>
                             Academics
                         </label>
 
                         <li>
-                            Course Registration
+                            <i>
+                                <FontAwesomeIcon icon={faRegistered} />
+                            </i>
+                            <span className='sidebar-menu-item'>Course Registration</span>
+                        </li>
+
+                        <li>
+                            <i>
+                                <FontAwesomeIcon icon={faCodePullRequest} />
+                            </i>
+                            <span className='sidebar-menu-item'>Academic Requistion</span>
+                        </li>
+
+                        <li>
+                            <i>
+                                <FontAwesomeIcon icon={faNoteSticky} />
+                            </i>
+                            <span className='sidebar-menu-item'>Course Evaluation</span>
                         </li>
                     </div>
 
-                    <div className='sidebar-content-title' id='financials-title'>
+                    <div className='sidebar-menu-title' id='financials-title'>
                         <label>
                             Financials
                         </label>
 
                         <li>
-                            Fee statement
+                            <i>
+                                <FontAwesomeIcon icon={faYenSign}  />
+                            </i>
+                            <span className='sidebar-menu-item'>Fee Statement</span>
+                        </li>
+
+                        <li>
+                            <i>
+                                <FontAwesomeIcon icon={faFolderMinus}  />
+                            </i>
+                            <span className='sidebar-menu-item'>Receipts</span>
+                        </li>
+
+                        <li>
+                            <i>
+                                <FontAwesomeIcon icon={faSterlingSign}  />
+                            </i>
+                            <span className='sidebar-menu-item'>Fee Structure</span>
+                        </li>
+
+                        <li>
+                            <i>
+                                <FontAwesomeIcon icon={faYenSign}  />
+                            </i>
+                            <span className='sidebar-menu-item'>Legacy Statement</span>
                         </li>
                     </div>
 
-                    <div className='sidebar-content-title' id='accomodation-title'>
+                    <div className='sidebar-menu-title' id='accomodation-title'>
                         <label>
                             Accomodation
                         </label>
 
                         <li>
-                            Hostel Booking
+                            <i>
+                                <FontAwesomeIcon icon={faBed}  />
+                            </i>
+                            <span className='sidebar-menu-item'>Hostel Booking</span>
                         </li>
                     </div>
 
-                    <div className='sidebar-content-title' id='examination-title'>
+                    <div className='sidebar-menu-title' id='examination-title'>
                         <label>
                             Examination
                         </label>
 
                         <li>
-                            Exam Card
+                            <i>
+                                <FontAwesomeIcon icon={faArrowsDownToLine}  />
+                            </i>
+                            <span className='sidebar-menu-item'>Exam Card</span>
+                        </li>
+
+                        <li>
+                            <i>
+                                <FontAwesomeIcon icon={faCloudArrowDown}  />
+                            </i>
+                            <span className='sidebar-menu-item'>Transcript</span>
                         </li>
                     </div>
 
-                    <div className='sidebar-content-title' id='settings-title'>
+                    <div className='sidebar-menu-title' id='settings-title'>
                         <label>
                             Settings
                         </label>
 
                         <li>
-                            Change Password
+                            <i>
+                                <FontAwesomeIcon icon={faGear}  />
+                            </i>
+                            <span className='sidebar-menu-item'>Settings</span>
                         </li>
                     </div>
-                    
                 </div>
             </React.Fragment>
         )

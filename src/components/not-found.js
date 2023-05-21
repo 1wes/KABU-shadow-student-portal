a1efc343337d6ec627b7d094a97fefbe14e00b67
1wes <okemwawes@gmail.com>
2023-05-21 23:36:36 +0300

    implement general layout of the 404 not found page

---
+++ src/components/not-found.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './not-found.css';
+
+class NotFound extends React.Component{
+
+    render(){
+
+        return(
+            <React.Fragment>
+                <div className='not-found-wrapper'>
+                    <div className='page-content'>
+                        <div className='header'>
+                            <img src={require('../kaba-name.png')} alt='Kabarak-logo' />
+                        </div>
+
+                        <div className='body-content'>
+                            <div className='decoration'>
+                                
+                            </div>
+                            <b className='main-text'>
+                                Page Not Found
+                            </b>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+export default NotFound;
